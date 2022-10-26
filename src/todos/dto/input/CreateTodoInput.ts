202375f4619c58da90c367fba3950c2de2361926
RunyShark <spartadragon117@live.com.mx>
2022-10-25 20:23:49 -0500

    classValitador

--- src/todos/dto/input/CreateTodoInput.ts
+++ src/todos/dto/input/CreateTodoInput.ts
@@ -1,7 +1,10 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 @InputType()
 export class CreateTodoInput {
   @Field(() => String)
+  @IsNotEmpty()
+  @IsString()
   description: string;
 }
